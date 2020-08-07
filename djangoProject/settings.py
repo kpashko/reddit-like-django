f63c5510c4c221aa44c6b898f6e596454c4bc82d
kostyapashko <pashkokostya@gmail.com>
2020-08-07 15:51:49 +0300

    checking settings

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -19,7 +19,9 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
